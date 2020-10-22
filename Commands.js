177c76943aeceb2008882259c2a5e6d278e514df
Runkified <anguyen5987@yahoo.com>
2020-10-22 16:30:02 -0400

    Separating Files
    Moved commands from Taskify.js to Commands.js. Still figuring out how to import/export functions from separate files.

---
+++ Commands.js
@@ -0,0 +1,19 @@
+/**
+ * We can add other commands to this file.
+ */
+
+/**
+ * Help command lists available commands.
+ */
+function helpCommand(arguments, receivedMessage) {
+    if (arguments.length > 0) {
+        receivedMessage.channel.send("You need help with `" + arguments + "`")
+    } else {
+        receivedMessage.channel.send("Try `t!help [category]`")
+    }
+}
+
+/**
+ * List of exported commands.
+ */
+export { helpCommand }
